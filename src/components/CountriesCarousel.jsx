bfaf7425f8c3830537862f5214785c1be3a544e2
Jo Wu <jo.wu@student.unsw.edu.au>
2025-04-19 01:04:56 +1000

    did the carousel

--- src/components/CountriesCarousel.jsx
+++ src/components/CountriesCarousel.jsx
@@ -1,23 +1,52 @@
 import lightsEastAsia from "../assets/lights-east-asia.png";
 import lightsUs from "../assets/lights-us.png";
 import lightsEurope from "../assets/lights-europe.png";
-import React from "react";
-import { CCarousel, CCarouselItem, CImage } from "@coreui/react";
+import { Box, Typography } from "@mui/material";
+// App.jsx or Carousel.jsx
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-const CountriesCarousel = () => {
+const images = [lightsEastAsia, lightsUs, lightsEurope];
+const countries = ["East Asia", "North America", "Europe"];
+
+function CountriesCarousel() {
   return (
-    <CCarousel controls>
-      <CCarouselItem>
-        <CImage className="d-block w-100" src={lightsEastAsia} alt="slide 1" />
-      </CCarouselItem>
-      <CCarouselItem>
-        <CImage className="d-block w-100" src={lightsUs} alt="slide 2" />
-      </CCarouselItem>
-      <CCarouselItem>
-        <CImage className="d-block w-100" src={lightsEurope} alt="slide 3" />
-      </CCarouselItem>
-    </CCarousel>
+    <Box width="400px" m="0 auto">
+      <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={10}
+        slidesPerView={1}
+        loop={true}
+        navigation
+        pagination={{ clickable: true }}
+        className="mySwiper"
+      >
+        {images.map((src, index) => (
+          <SwiperSlide key={index}>
+            <Box
+              width="100%"
+              position="relative"
+              display={"flex"}
+              justifyContent={"center"}
+            >
+              <Box
+                component="img"
+                src={src}
+                alt={`Slide ${index + 1}`}
+                width="100%"
+              />
+              <Typography position="absolute" zIndex={1} bottom="25px">
+                {countries[index]}
+              </Typography>
+            </Box>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
   );
-};
+}
 
 export default CountriesCarousel;
